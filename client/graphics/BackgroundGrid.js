ff4a4c355eb84c3b3ed2fb95d8e6fb21824fe8eb
Highfire1 <tseng.andersonn@gmail.com>
2021-08-29 06:04:55 -0700

    final commit #2

--- client/graphics/BackgroundGrid.js
+++ client/graphics/BackgroundGrid.js
@@ -3,7 +3,33 @@ import { Container, Graphics } from 'pixi.js'
 class BackgroundGrid extends Container {
     constructor() {
         super()
+        
+        //this.fillStyle="rgb(0,0,255)"
+        //var canvas = document.getElementById("canvas");
+        //var ctx = this.getContext("2d");
+        //ctx.fillStyle = "blue";
+        //ctx.fillRect(0, 0, this.width, this.height);
+        
+        let circle = new Graphics()
+        circle.lineStyle(2, 0x333333)
+        circle.drawCircle(0, 0, 100)
+        circle.fillStyle = "blue"; // ????
+        this.addChild(circle)
+        
+        // pain tm
+        circle.drawCircle(500, -240, 200)
+        this.addChild(circle)
 
+        circle.drawCircle(300, 300, 20)
+        this.addChild(circle)
+
+        circle.drawCircle(-30, -400, 50)
+        this.addChild(circle)
+
+        circle.drawCircle(-600, 100, 300)
+        this.addChild(circle)
+
+        /*
         for (let i = 0; i < 11; i++) {
             let line = new Graphics()
             line.lineStyle(2, 0x333333)
@@ -19,6 +45,7 @@ class BackgroundGrid extends Container {
             line.lineTo(1000, i * 100)
             this.addChild(line)
         }
+        */
     }
 }
 
--- public/js/script.js
+++ public/js/script.js
@@ -10,8 +10,13 @@ button.id = "button"
 var body = document.getElementById("body")
 body.appendChild(button);
 
-button.addEventListener("click", account_dia);
+button.addEventListener("click", account_info);
 
-function account_dia() {
-    alert("BUTTON!")
+button.blur = true // why does this not work ;-;
+
+function account_info() {
+    window.location.replace("https://discord.com/api/oauth2/authorize?client_id=881453537396129792&redirect_uri=https%3A%2F%2Fboundlessocean.tech%2F&response_type=code&scope=email")
 }
+
+
+document.getElementById("main-canvas").blur();
